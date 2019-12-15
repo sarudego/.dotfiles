43bfac034020546e229da5c8565110a844f91f65
sarudego <sruizdegopegui@gmail.com>
2019-12-16 00:47:28 +0100

    Check if files are executable

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+function isExec {
+  if [[ -f "$1" && -x $("$1") ]]; then
+        true;
+  else
+        echo "Making $1 executable...";
+	chmod +x $1
+  fi;
+}
+
+isExec symlink.sh
+isExec pacinstall.sh
+isExec programs.sh
+
 ./symlink.sh
 ./pacinstall.sh
 ./programs.sh
